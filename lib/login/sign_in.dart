769b8e01ce0a7aa17bcb978d3f2c959458c106c1
Fathur Radhy <radhsyn83@gmail.com>
2023-08-07 13:53:19 +0700

    add sigin page

--- lib/login/sign_in.dart
+++ lib/login/sign_in.dart
@@ -5,6 +5,11 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back_ios_new),
+      ),
+      body: Column(),
+    );
   }
 }
